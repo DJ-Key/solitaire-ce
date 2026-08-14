22e1ed057f52914474dbd7b6de8548ac7b6d035b
DJ-Key <36241764+DJ-Key@users.noreply.github.com>
2026-08-14 13:09:02 +0200

    Update index.php

--- templates/index.php
+++ templates/index.php
@@ -1,7 +1,7 @@
 <?php
-script('solitaire-ce', 'cards');
-script('solitaire-ce', 'main');
-style('solitaire-ce', 'sol');
+script('solitaire_ce', 'cards');
+script('solitaire_ce', 'main');
+style('solitaire_ce', 'sol');
 ?>
 
 <?php print_unescaped($this->inc('content/index')); ?>
